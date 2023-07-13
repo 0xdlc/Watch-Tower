a71e07b5a314d15363a3cfe2009919e8e9794f59
saint pablo <127287722+0xdlc@users.noreply.github.com>
2023-07-13 22:41:05 +0330

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -2,6 +2,7 @@ import argparse
 from utils.loader import FindDif
 from utils.loader import insert_Program
 from utils.loader import sub_only
+from utils.loader import costume_httpx
 from pymongo import MongoClient
 def main():
     # CONNECT TO MONGO db
@@ -12,24 +13,31 @@ def main():
     parser.add_argument('-p', required=True, default=False, metavar='programs to watch', type=str)
     parser.add_argument('-scope',required=False,nargs='?', const='true', metavar='Watch for new scope', type=bool)
     parser.add_argument('-sub',required=False,nargs='?', const='true', metavar='Watch for new subdomains', type=bool)
+    parser.add_argument('-httpx',required=False,nargs='?', const='true', metavar='Watch for httpx diffrences', type=bool)
+    parser.add_argument('-costume_httpx', required=False, default=False, metavar='choose a file to watch for httpx', type=str)
     args = parser.parse_args()
-    sub = False
-    cidr = False
     programs = args.p.split(',')
     print("[+] selected programs are:" + str(programs))
+
+    
+
     for i in programs:
        query = {"program": f"{i}"}
        if collection.find_one(query) != None:
-             if args.get:
-                  print(f"getting assets of {i}")
-                  for j in collection.distinct("assets", {"program": f"{i}"}):
-                       print(j)
-             if args.sub:
-                  sub_only(i)          
-             if args.scope:
-                  print(f'[+] program {i} found in the database')
-                  FindDif(i)                       
+          if args.get:
+               print(f"getting assets of {i}")
+               for j in collection.distinct("assets", {"program": f"{i}"}):
+                    print(j)
+          if args.sub:
+               sub_only(i)          
+          if args.scope:
+               print(f'[+] program {i} found in the database')
+               FindDif(i)                       
        else:
-             insert_Program(i,args.sub)
+          insert_Program(i,args.sub)
+    if args.costume_httpx:
+       costume_httpx(args.costume_httpx)
+
+          
 if __name__ == '__main__':
     main()
